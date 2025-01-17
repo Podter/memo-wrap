987246ef5d524c9abbdc2cb72f9e675da1a72005
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2025-01-18 03:27:45 +0700

    Update redis.ts

--- src/driver/redis.ts
+++ src/driver/redis.ts
@@ -2,30 +2,38 @@ import type { Redis } from "ioredis";
 
 import { defineDriver } from "./builder";
 
-export const redisDriver = defineDriver<string, Redis>((redis) => {
-  return {
-    async get(key) {
-      return await redis.get(key);
-    },
-    async set(key, value, ttl) {
-      await redis.set(key, value);
-      if (ttl) {
-        await redis.expire(key, ttl);
-      }
-    },
-  };
-});
+interface RedisDriverOpts {
+  redis: Redis;
+}
 
-export const redisBufferDriver = defineDriver<Buffer, Redis>((redis) => {
-  return {
-    async get(key) {
-      return await redis.getBuffer(key);
-    },
-    async set(key, value, ttl) {
-      await redis.set(key, value);
-      if (ttl) {
-        await redis.expire(key, ttl);
-      }
-    },
-  };
-});
+export const redisDriver = defineDriver<string, RedisDriverOpts>(
+  ({ redis }) => {
+    return {
+      async get(key) {
+        return await redis.get(key);
+      },
+      async set(key, value, ttl) {
+        await redis.set(key, value);
+        if (ttl) {
+          await redis.expire(key, ttl);
+        }
+      },
+    };
+  },
+);
+
+export const redisBufferDriver = defineDriver<Buffer, RedisDriverOpts>(
+  ({ redis }) => {
+    return {
+      async get(key) {
+        return await redis.getBuffer(key);
+      },
+      async set(key, value, ttl) {
+        await redis.set(key, value);
+        if (ttl) {
+          await redis.expire(key, ttl);
+        }
+      },
+    };
+  },
+);
